17039a9288f50bfc6ee718f60f1c86cf1e13f5c6
madalapranu <madalapranu@gmail.com>
2023-10-14 15:36:31 -0400

    Created the Nova Screen Basic UI

--- AwesomeProject/App2.tsx
+++ AwesomeProject/App2.tsx
@@ -1,13 +1,30 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {
+  View, 
+  Text,
+  StyleSheet,
+  Button} from 'react-native';
 
 const ViewBoxesWithColorAndText = () => {
   return (
     <View>
-      <Text>This item is</Text>
-      <Text>Category</Text>
+      <Text style={{fontSize: 30, textAlign: 'center', marginTop: 10, color: 'black'}}>This item is</Text>
+      <Text style={{fontSize: 50, textAlign: 'center', marginTop: 10, color: '#CF142B', fontWeight: 'bold'}}>Ultraprocessed</Text>
+      <Text>{"\n"}</Text>
+      <Text>{"\n"}</Text>
+      <Text style={{fontSize: 30, textAlign: 'center', marginTop: 10, color: 'black'}}>Suggested Alternatives</Text>
+      <Text>{"\n"}</Text>
+      <Text>{"\n"}</Text>
+      <View style={{flexDirection: 'row',marginLeft:70}}>
+        <Text style={{fontSize: 30, color: 'black'}}>Option 1    </Text>
+        <Text style={{fontSize: 30, color: 'black'}}>Option 2</Text>
+      </View>
+      <Text>{"\n"}</Text>
+      <Text>{"\n"}</Text>
+      <Button title = "Add to Grocery List"></Button>
     </View>
   );
 };
 
 export default ViewBoxesWithColorAndText;
+
